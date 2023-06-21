6858387dfa8cd9c357e208b70e0173fae91ab54a
Sanket Mane <111991599+Sanketmane2323@users.noreply.github.com>
2023-06-21 20:25:53 +0530

    Color changer website

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let element=document.getElementById("Content");
+function indianred(){
+    element.style.backgroundColor="#cd5c5c"
+}
+
+
+function lightblue(){
+    element.style.backgroundColor="#add8e6"
+}
+
+function lightgray(){
+    element.style.backgroundColor="#d3d3d3"
+}
+
+function lime(){
+    element.style.backgroundColor="#00ff00"
+}
+
+function red(){
+    element.style.backgroundColor="red"
+}
+
+function blue(){
+    element.style.backgroundColor="blue"
+}
+
+function orange(){
+    element.style.backgroundColor="orange"
+}
+
+function pink(){
+    element.style.backgroundColor="pink"
+}
+
+function lavendar(){
+    element.style.backgroundColor="#e6e6fa"
+}
+
+
